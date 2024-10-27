a64a90c200fb765058dc6bbe8eb0bc90083e88a2
Reece Hagan <drogonmar@tuyuji.com>
2024-10-26 18:33:03 -0700

    Correct def file.
    Signed-off-by: Reece Hagan <drogonmar@tuyuji.com>

--- ako.d.ts
+++ ako.d.ts
@@ -2,25 +2,30 @@ type JSONValue =
   | string
   | number
   | boolean
+  | null
+  | { [key: string]: JSONValue }
+  | JSONValue[];
+
+type AkoRootObj =
   | { [key: string]: JSONValue }
   | JSONValue[];
 
 /**
- * Serializes a JSON-compatible object or array into a formatted string.
- * @param thing - The JSON-compatible object or array to serialize.
+ * Serializes a JSON object or array into a formatted string.
+ * @param obj - The JSON object or array to serialize.
  * @param do_formatting - Optional. Whether to apply formatting (default: true).
  * @param use_spaces - Optional. If `do_formatting` is true, determines whether to use spaces instead of tabs (default: false).
  * @returns A formatted string representation of the input.
  */
 export function serialize(
-  thing: JSONValue,
+  obj: AkoRootObj,
   do_formatting?: boolean,
   use_spaces?: boolean,
 ): string;
 
 /**
- * Parses an Ako-formatted string into a JavaScript object or array.
- * @param src - The Ako-formatted string to parse.
+ * Parses an Ako string into a JavaScript object or array.
+ * @param src - The Ako string to parse.
  * @returns A JavaScript object, array, or null if the parsing fails.
  */
-export function parse(src: string): JSONValue | null;
+export function parse(src: string): AkoRootObj | null;
